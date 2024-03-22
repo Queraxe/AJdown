9dacf991214beca11b37621ad5af686d3127c8d6
Queraxe <adrian.jonkov@gmail.com>
2024-03-22 23:34:38 +0100

    alles super

--- script.js
+++ script.js
@@ -6,15 +6,24 @@ input.addEventListener("input", myFunction);
 var timetodate = new Date();
 
 function myFunction() {
-	console.log(input.textContent);
 	timetovalue = input.value;
 
 	const timearray = timetovalue.split(":");
 
 	timetodate.setHours(timearray[0]);
 	timetodate.setMinutes(timearray[1]);
+	timetodate.setSeconds(0);
+	timetodate.setMilliseconds(0);
 
-	update();
+	var now = new Date();
+
+	if (now > timetodate) {
+		timetodate.setDate(now.getDate() + 1);
+	}
+
+	setInterval(function () {
+		update();
+	}, 1);
 }
 
 function update() {
@@ -22,17 +31,25 @@ function update() {
 
 	var timediff = timetodate - timenow;
 
-	var stunden = Math.floor(timediff / (1000 * 60 * 60));
-	var minuten = Math.floor((timediff % (1000 * 60 * 60)) / (1000 * 60));
-	var sekunden = Math.floor((timediff % (1000 * 60)) / 1000);
-	var millisekunden = Math.floor(timediff % 1000);
+	var stunden = (Math.floor(timediff / (1000 * 60 * 60)) / 100)
+		.toFixed(2)
+		.split(".")[1];
+	var minuten = (
+		Math.floor((timediff % (1000 * 60 * 60)) / (1000 * 60)) / 100
+	)
+		.toFixed(2)
+		.split(".")[1];
+	var sekunden = (Math.floor((timediff % (1000 * 60)) / 1000) / 100)
+		.toFixed(2)
+		.split(".")[1];
+	var millisekunden = (Math.floor(timediff % 1000) / 1000)
+		.toFixed(3)
+		.split(".")[1];
 
-	console.log();
-
-	timer.innerHTML =
-		stunden + ":" + minuten + ":" + sekunden + ":" + millisekunden;
+	if (timediff <= 0) {
+		timer.innerHTML = "STOP";
+	} else {
+		timer.innerHTML =
+			stunden + ":" + minuten + ":" + sekunden + ":" + millisekunden;
+	}
 }
-
-setInterval(function () {
-	update();
-}, 1);
